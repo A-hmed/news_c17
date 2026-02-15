5933c11bb988ea85f45dd529f0b4865f8314e41d
Ahmed Nabil <ahmed.nabil@kazyon.com>
2026-02-15 21:50:14 +0200

    Add categories screen

--- lib/ui/screens/navigation/tabs/news/news_tab.dart
+++ lib/ui/screens/navigation/tabs/news/news_tab.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:news_c17/apis/api_manager.dart';
 import 'package:news_c17/model/category.dart';
@@ -5,23 +7,45 @@ import 'package:news_c17/model/source.dart';
 import 'package:news_c17/ui/screens/navigation/tabs/news/news_list.dart';
 import 'package:news_c17/ui/widgets/app_error_widget.dart';
 
-class NewsTab extends StatelessWidget {
+class NewsTab extends StatefulWidget {
   final AppCategory category;
+
   const NewsTab(this.category, {super.key});
 
   @override
+  State<NewsTab> createState() => _NewsTabState();
+}
+
+class _NewsTabState extends State<NewsTab> {
+  NewsViewModel viewModel = NewsViewModel();
+  @override
+  void initState() {
+    super.initState();
+    viewModel.loadSources(widget.category.name);
+  }
+  @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: ApiManager.loadSources(category.name),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return AppErrorWidget(message: snapshot.error.toString());
-          } else if (snapshot.hasData) {
-            return buildTabsList(snapshot.data!);
-          } else {
+    return StreamBuilder(
+        stream: viewModel.controller.stream,
+        builder: (context, snapshot){
+          if(snapshot.data == null){
             return Center(child: CircularProgressIndicator());
+          }else {
+            return buildTabsList(snapshot.data!);
           }
         });
+
+    // return FutureBuilder(
+    //     future: ApiManager.loadSources(widget.category.name),
+    //     builder: (context, snapshot) {
+    //       if (snapshot.hasError) {
+    //         return AppErrorWidget(message: snapshot.error.toString());
+    //       } else if (snapshot.hasData) {
+    //         return buildTabsList(snapshot.data!);
+    //       } else {
+    //         return Center(child: CircularProgressIndicator());
+    //       }
+    //     });
   }
 
   buildTabsList(List<Source> sources) {
@@ -48,5 +72,15 @@ class NewsTab extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+
+
+class NewsViewModel{
+  StreamController<List<Source>?> controller = StreamController();
+  loadSources(String category) async {
+    var sources = await ApiManager.loadSources(category);
+    controller.add(sources);
   }
 }
